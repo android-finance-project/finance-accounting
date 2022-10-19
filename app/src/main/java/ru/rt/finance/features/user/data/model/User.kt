0680e6f2f5a4ba04b575b5ab207e53e422859352
Tretyakov Vyacheslav <tretiakovslava@gmail.com>
2022-10-19 21:25:08 +0300

    Создание дефолтных моделей и readme (#3)
    * Create README.md
    
    * Дефолтные сущности и расположение по пакетам

---
+++ app/src/main/java/ru/rt/finance/features/user/data/model/User.kt
@@ -0,0 +1,19 @@
+package ru.rt.finance.features.user.data.model
+
+/**
+ * Пользователь
+ *
+ */
+data class User(
+    /**
+     *
+     */
+    val id: Int,
+    /**
+     *
+     */
+    val name: String? = null,
+    //val surname: String? = null,
+    //val patronimic: String? = null,
+    //val flagCommandante: Int? = null,
+)
